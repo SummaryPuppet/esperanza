5a3767bf7b19a9aecb29a00e85eb493284f116d6
SummaryPuppet <adrianm_sv@hotmail.com>
2025-05-04 00:12:37 -0500

    add elevenlabs provider

--- src/services/voice/index.ts
+++ src/services/voice/index.ts
@@ -1,5 +1,14 @@
+import { textToSpeech as textToSpeechElevenLabs } from "./elevenlabs";
 import { textToSpeech as textToSpeechSay } from "./say";
 
 export function textToSpeech(text: string) {
-  textToSpeechSay(text);
+  const provider = process.env.VOICE_PROVIDER || "say";
+  switch (provider) {
+    case "elevenlabs":
+      return textToSpeechElevenLabs(text);
+    case "say":
+      return textToSpeechSay(text);
+    default:
+      throw new Error(`Unsupported voice provider: ${provider}`);
+  }
 }
