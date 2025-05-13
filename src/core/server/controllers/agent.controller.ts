354749576d3e16ef137673a1dd6827ec69676921
SummaryPuppet <adrianm_sv@hotmail.com>
2025-05-12 22:10:13 -0500

    add text to speech

--- src/core/server/controllers/agent.controller.ts
+++ src/core/server/controllers/agent.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Agent } from "../../../services/agent/Agent";
+import { textToSpeech } from "../../../services/voice";
 
 export const agentController = async (req: Request, res: Response) => {
   const { question } = req.body;
@@ -7,6 +8,7 @@ export const agentController = async (req: Request, res: Response) => {
   const agent = Agent.getInstance();
 
   const answer = await agent.think(question);
+  textToSpeech(answer);
 
   res.json({
     message: `Question received: ${question}`,
